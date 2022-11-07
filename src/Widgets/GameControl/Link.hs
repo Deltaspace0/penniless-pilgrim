6ce07867a000882cbc8b7f875b080b54d1279ef6
deltaspace0 <rgadeew@yandex.ru>
2022-11-07 18:02:48 +0300

    feat game control link animation

--- src/Widgets/GameControl/Link.hs
+++ src/Widgets/GameControl/Link.hs
@@ -15,6 +15,7 @@ module Widgets.GameControl.Link
     ) where
 
 import Control.Lens
+import Control.Monad
 import Data.Default
 import Data.Maybe
 import Monomer
@@ -29,12 +30,6 @@ data LinkForm = LinkBack | LinkForward deriving (Eq, Show)
 data Link = Link
     { _linkColor :: Color
     , _linkForm :: Maybe LinkForm
-    } deriving (Eq, Show)
-
-data LinkData = LinkData
-    { _ldLink :: Maybe Link
-    , _ldColor :: Color
-    , _ldNodeToWidthRatio :: Double
     } deriving (Eq, Show)
 
 makeFields 'Link
@@ -61,35 +56,114 @@ vlinkTransform colors (Just G.LinkForward) = Just Link
     , _linkForm = Just LinkForward
     }
 
+data LinkData = LinkData
+    { _ldLink :: Maybe Link
+    , _ldColor :: Color
+    , _ldNodeToWidthRatio :: Double
+    } deriving (Eq, Show)
+
+data LinkState = LinkState
+    { _lsLink :: Maybe Link
+    , _lsOldLink :: Maybe Link
+    , _lsRunning :: Bool
+    , _lsStart :: Millisecond
+    } deriving (Eq, Show)
+
+instance Default LinkState where
+    def = LinkState
+        { _lsLink = Nothing
+        , _lsOldLink = Nothing
+        , _lsRunning = False
+        , _lsStart = 0
+        }
+
 gameControlHlink :: LinkData -> WidgetNode s e
 gameControlHlink linkData = node where
     node = defaultWidgetNode "gameControlLink" widget
-    widget = makeGameControlLink True linkData
+    widget = makeGameControlLink True linkData def
 
 gameControlVlink :: LinkData -> WidgetNode s e
 gameControlVlink linkData = node where
     node = defaultWidgetNode "gameControlLink" widget
-    widget = makeGameControlLink False linkData
+    widget = makeGameControlLink False linkData def
 
-makeGameControlLink :: Bool -> LinkData -> Widget s e
-makeGameControlLink isHz linkData = widget where
-    widget = createSingle () def
-        { singleRender = render
+makeGameControlLink :: Bool -> LinkData -> LinkState -> Widget s e
+makeGameControlLink isHz linkData state = widget where
+    widget = createSingle state def
+        { singleInit = init
+        , singleMerge = merge
+        , singleRender = render
         }
 
+    init _ node = resultNode resNode where
+        resNode = node & L.widget .~ w
+        w = makeGameControlLink isHz linkData state'
+        state' = state
+            { _lsLink = _ldLink linkData
+            , _lsOldLink = _ldLink linkData
+            }
+
+    merge wenv newNode _ oldState = result where
+        result = resultReqs resNode reqs
+        resNode = newNode & L.widget .~ w
+        w = makeGameControlLink isHz linkData state'
+        state' = if newLink == oldLink
+            then oldState
+            else LinkState
+                { _lsLink = newLink
+                , _lsOldLink = oldLink
+                , _lsRunning = True
+                , _lsStart = newStart
+                }
+        reqs = if newLink == oldLink
+            then []
+            else [RenderEvery widgetId period (Just steps)]
+        newLink = _ldLink linkData
+        oldLink = _lsLink oldState
+        oldStart = _lsStart oldState
+        newStart = ts - 500 + (min 500 (ts-oldStart))
+        ts = wenv ^. L.timestamp
+        widgetId = newNode ^. L.info . L.widgetId
+        period = 10
+        steps = fromIntegral $ 500 `div` period
+
     render wenv node r = do
-        let link = _ldLink linkData
+        let ts = wenv ^. L.timestamp
+            newLink = _lsLink state
+            oldLink = _lsOldLink state
+            running = _lsRunning state
+            start = _lsStart state
             style = currentStyle wenv node
-            Rect x y linkSize nodeSize = getContentArea node style
-            Just link' = link
-            (color', form') = if null link
+            contentArea = getContentArea node style
+            delta = fromIntegral $ ts-start
+            progress = max 0 $ min 1 $ delta/500
+            Just newLink' = newLink
+            Just oldLink' = oldLink
+            newData@(newColor, newForm) = if null newLink
                 then (_ldColor linkData, Nothing)
-                else (link' ^. color, link' ^. form)
+                else (newLink' ^. color, newLink' ^. form)
+            oldData@(oldColor, oldForm) = if null oldLink
+                then (_ldColor linkData, Nothing)
+                else (oldLink' ^. color, oldLink' ^. form)
+        if running && progress < 1
+            then if null newForm
+                then renderForm r oldData contentArea (1-progress)
+                else renderForm r newData contentArea progress
+            else renderForm r newData contentArea 1
+
+    renderForm r (color', form') contentArea progress = do
+        let Rect x y linkSize nodeSize = contentArea
             c = Just color'
-            s = if isHz then x else y
-            b = s + linkSize - nodeSize
+            totalSize = linkSize-nodeSize*2
+            totalSize' = totalSize*progress
+            rest = totalSize-totalSize'
+            s' = nodeSize + if isHz then x else y
+            s = s' + if form' == Just LinkBack then rest else 0
+            b = s+totalSize'
             linkWidth = nodeSize/(_ldNodeToWidthRatio linkData)
-            ars = linkWidth*1.5
+            ars = if totalSize' > linkWidth*3
+                then linkWidth*1.5
+                else totalSize'/2
             drawLine' a b = drawLine r p1 p2 linkWidth c where
                 p1 = if isHz then Point a y else Point x a
                 p2 = if isHz then Point b y else Point x b
@@ -97,17 +171,17 @@ makeGameControlLink isHz linkData = widget where
                 p1 = if isHz then Point a y else Point x a
                 p2 = if isHz then Point b y1 else Point x1 b
                 p3 = if isHz then Point b y2 else Point x2 b
-                x1 = x + ars
-                x2 = x - ars
-                y1 = y - ars
-                y2 = y + ars
+                x1 = x+ars
+                x2 = x-ars
+                y1 = y-ars
+                y2 = y+ars
         case form' of
             Just LinkBack -> do
-                let b' = s + nodeSize + ars*2
-                drawLine' (b' - 1) (b + 1)
-                drawTriangle' (s + nodeSize) b'
+                let b' = s+ars*2
+                drawLine' (b'-1) b
+                drawTriangle' s b'
             Just LinkForward -> do
-                let b' = b - ars*2
-                drawLine' (s + nodeSize - 1) (b' + 1)
+                let b' = b-ars*2
+                drawLine' s (b'+1)
                 drawTriangle' b b'
-            _ -> drawLine' (s + nodeSize - 1) (b + 1)
+            _ -> drawLine' s b
